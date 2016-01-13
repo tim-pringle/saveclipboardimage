493c4b05ea4fa9014cbc0d138c504c1b65fd25ef
Tim Pringle <tim.pringle@saasplaza.com>
2016-01-13 11:35:55 +0100

    Saved clipboard image script to file

---
+++ source/Save-ClipboardImage.ps1
@@ -0,0 +1,12 @@
+﻿Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -Action {
+    $content = Get-Clipboard -Format Image
+    If ($content) 
+    {
+        $path = $psISE.CurrentFile.FullPath | Split-Path
+        $filename = "$path\ImageCap - " + (Get-date -Format 'hh-mm-ss') + '.png'
+        $content.Save($filename,'png')
+        Set-Clipboard -Value $Null
+    }
+} 
+
+
